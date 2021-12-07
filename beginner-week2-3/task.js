baa0e1d30619fd05efe97513be74084a1585e752
OSEMUDIAMEN ITUA <osemudiamenitua@gmail.com>
2021-12-07 22:18:35 +0100

    feat: complete the tasks

--- beginner-week2-3/task.js
+++ beginner-week2-3/task.js
@@ -1,6 +1,17 @@
 const URL = 'https://v2.jokeapi.dev/joke/Any?idRange=180';
+const fs = require('fs');
+const axios = require('axios');
+
 const isPrime = (num) => {
   //complete the function to return true or false if a numnber is prime or not.
+  let i = 2;
+  while (i * i < num * 2) {
+    if (num % i == 0) {
+      return false;
+    }
+    i++;
+  }
+  return true;
 };
 
 const writeToFile = (a, b) => {
@@ -14,15 +25,31 @@ const writeToFile = (a, b) => {
   // 13
   // 17
   // You will likely use the isPrime function above.
+
+  let data = '';
+
+  for (let i = a; i <= b; i++) {
+    if (isPrime(i)) {
+      data = data + i + '\n';
+    }
+  }
+
+  fs.writeFile('myFile.txt', data, (err) => {
+    console.log(err);
+  });
 };
 
-const getJoke = () => {
+const getJoke = async () => {
   // Using axios or any other method of your preference
   // make a get request to https://v2.jokeapi.dev/joke/Any?idRange=180
   // it returns a json object.
   // get the "joke" parameter in the json object.
   //return that value.
+
+  const { data } = await axios.get(URL);
+  return data.joke;
 };
+writeToFile(3, 100);
 
 module.exports = {
   isPrime,
